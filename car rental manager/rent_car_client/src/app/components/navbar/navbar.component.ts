18b140735bc3db140dea16a0cc78b40cb95d7610
letuankhoi.dev <letuankhoi.dev@gmail.com>
2021-12-17 08:13:40 +0700

    fix: hidden my contract when logout

--- car rental manager/rent_car_client/src/app/components/navbar/navbar.component.ts
+++ car rental manager/rent_car_client/src/app/components/navbar/navbar.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { HostListener, PLATFORM_ID, Inject } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { HostListener, PLATFORM_ID, Inject } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor(@Inject(PLATFORM_ID) public platformId: string, private router: Router) { }
+  constructor(
+    @Inject(PLATFORM_ID) public platformId: string,
+    private router: Router
+  ) {}
 
   public scrolled = false;
   public isAuth = false;
   public awake = false;
   public display = true;
   public stretching = false;
-  public ltkLogo = 'assets/logo/las.png';
+  public ltkLogo = "assets/logo/las.png";
   username;
   role;
+  public isDisplayContract: boolean;
 
-  @HostListener('window:scroll', [])
+  @HostListener("window:scroll", [])
   onWindowScroll() {
     if (isPlatformBrowser(this.platformId)) {
       this.scrolled = window.scrollY > 30;
@@ -29,7 +32,7 @@ export class NavbarComponent implements OnInit {
       if (window.scrollY > 30) {
         this.display = true;
       } else {
-        this.ltkLogo = '/assets/logo/las.png';
+        this.ltkLogo = "/assets/logo/las.png";
       }
     }
   }
@@ -38,7 +41,7 @@ export class NavbarComponent implements OnInit {
     window.scroll({
       top: 0,
       left: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   }
 
@@ -51,24 +54,23 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit() {
-    const TOKEN = localStorage.getItem('TOKEN');
-    this.username = localStorage.getItem('USERNAME');
-    this.role = localStorage.getItem('ROLE');
+    const TOKEN = localStorage.getItem("TOKEN");
+    this.username = localStorage.getItem("USERNAME");
+    this.role = localStorage.getItem("ROLE");
     if (TOKEN) {
       this.isAuth = true;
-    }
-    else {
+    } else {
       this.isAuth = false;
     }
   }
 
   onLogout() {
     localStorage.clear();
-    this.router.navigate(['/'])
+    this.router.navigate(["/"]);
+    const username = localStorage.getItem("USERNAME");
+    this.isDisplayContract = username ? true : false;
     setTimeout(() => {
-      location.reload()
+      location.reload();
     }, 1000);
   }
-
-
 }
